67f67c4f20623268931ff156689e213ec8fa6a23
davilivob <davilivob@gmail.com>
2024-05-03 21:38:28 +0800

    Add a basic OSC signal receiver

--- src/routes/api/progress/+server.ts
+++ src/routes/api/progress/+server.ts
@@ -1,4 +1,15 @@
 import type { RequestHandler } from './$types';
+import { Server } from 'node-osc';
+import { OSC_PORT } from '$env/static/private';
+
+const oscReceiver = new Server(parseInt(OSC_PORT) || 30000, '127.0.0.1', () => {
+	console.log('OSC Server is listening');
+});
+
+oscReceiver.on('message', (msg) => {
+	console.log(`Message: ${msg}`);
+});
+
 import { value } from '$lib/progress';
 import { json } from '@sveltejs/kit';
 
